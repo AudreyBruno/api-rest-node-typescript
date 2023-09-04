0c061523cedbfc86a44c23b229b0e5b2f5062b6c
Audrey Bruno <audreysiqueiracvv@gmail.com>
2023-09-04 15:01:39 -0300

    feat: added yup validation to cidades create controller

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import { CidadesController } from './../controllers';
 
 const router = Router();
 
 router.get('/', (_, res) => {
-  return res.send('Olá dev');
+  return res.send('Chamada realizada com sucesso');
 });
 
-router.post('/teste', (req, res) => {
-  return res.sendStatus(StatusCodes.UNAUTHORIZED).json(req.body);
-});
+router.post('/cidades', CidadesController.createValidation, CidadesController.create);
 
 export { router };
