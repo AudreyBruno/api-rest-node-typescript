d332138393a4af5d81fac084978229f08c926be5
Audrey Bruno <audreysiqueiracvv@gmail.com>
2023-09-11 16:44:14 -0300

    feat: added cidades provider method to criate cidades

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -4,17 +4,28 @@ import { StatusCodes } from 'http-status-codes';
 
 import { validation } from '../../shared/middleware';
 import { ICidade } from '../../database/models';
+import { CidadesProvaider } from '../../database/providers/cidades';
 
 interface IBodyProps extends Omit<ICidade, 'id'> {}
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(yup.object().shape({
-    nome: yup.string().required().min(3),
+    nome: yup.string().required().min(3).max(150),
   })),
 }));
 
 export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
 
-  return res.status(StatusCodes.CREATED).json(1);
+  const result = await CidadesProvaider.create(req.body);
+
+  if (result instanceof Error){
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message
+      }
+    });
+  }
+
+  return res.status(StatusCodes.CREATED).json(result);
   
 };
