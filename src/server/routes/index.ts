8925e9e2773f4e519ff403e34c48d5f1d2caa600
Audrey Bruno <audreysiqueiracvv@gmail.com>
2023-09-13 16:15:59 -0300

    feat: added controllers to pessoas

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { CidadesController } from './../controllers';
+import { CidadesController, PessoasController } from './../controllers';
 
 const router = Router();
 
@@ -9,11 +9,14 @@ router.get('/', (_, res) => {
 
 router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
 router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById);
-
 router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById);
-
 router.post('/cidades', CidadesController.createValidation, CidadesController.create);
-
 router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
+
+router.get('/pessoas', PessoasController.getAllValidation, PessoasController.getAll);
+router.get('/pessoas/:id', PessoasController.getByIdValidation, PessoasController.getById);
+router.delete('/pessoas/:id', PessoasController.deleteByIdValidation, PessoasController.deleteById);
+router.post('/pessoas', PessoasController.createValidation, PessoasController.create);
+router.put('/pessoas/:id', PessoasController.updateByIdValidation, PessoasController.updateById);
 
 export { router };
