a2e6334b4a06e10fe00478ee1ffc98e2a178d96c
Audrey Bruno <audreysiqueiracvv@gmail.com>
2023-09-15 16:14:54 -0300

    feat: added authentication with JWT

--- src/server/controllers/usuarios/SingIn.ts
+++ src/server/controllers/usuarios/SingIn.ts
@@ -5,7 +5,7 @@ import { StatusCodes } from 'http-status-codes';
 import { validation } from '../../shared/middleware';
 import { IUsuario } from '../../database/models';
 import { UsuariosProvider } from '../../database/providers/usuarios';
-import { PasswordCrypto } from '../../shared/services';
+import { JWTService, PasswordCrypto } from '../../shared/services';
 
 interface IBodyProps extends Omit<IUsuario, 'id' | 'nome'> {}
 
@@ -20,9 +20,9 @@ export const singIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
 
   const { email, senha } = req.body;
 
-  const result = await UsuariosProvider.getByEmail(email);
+  const usuario = await UsuariosProvider.getByEmail(email);
 
-  if (result instanceof Error){
+  if (usuario instanceof Error){
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
         default: 'Email ou senha são inválidos'
@@ -30,7 +30,7 @@ export const singIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
     });
   }
 
-  const passwordMatch = await PasswordCrypto.verifyPassword(senha, result.senha);
+  const passwordMatch = await PasswordCrypto.verifyPassword(senha, usuario.senha);
   if (!passwordMatch){
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
@@ -38,7 +38,16 @@ export const singIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
       }
     });
   } else {
-    return res.status(StatusCodes.OK).json({ accessToken: 'teste.teste.teste'});
+    const accessToken = JWTService.sign({ uid: usuario.id });
+    if (accessToken === 'JWT_SECRET_NOT_FOUND'){
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        errors: {
+          default: 'Erro ao gerar o token de acesso'
+        }
+      });
+    }
+
+    return res.status(StatusCodes.OK).json({ accessToken });
   }
   
 };
